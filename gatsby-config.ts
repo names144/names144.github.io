868556e8f8649d07b0f8bd61ba5fc89aaba209fc
Nick Ames <names144@gmail.com>
2022-07-19 01:24:19 +0000

    💄 Add Chakra UI

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -3,25 +3,40 @@ import type { GatsbyConfig } from "gatsby";
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `Nick Ames`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://www.yourdomain.tld`,
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: ["gatsby-plugin-emotion", "gatsby-plugin-image", "gatsby-plugin-react-helmet", {
-    resolve: 'gatsby-plugin-manifest',
-    options: {
-      "icon": "src/images/icon.png"
-    }
-  }, "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
+  plugins: [
+    "gatsby-plugin-emotion",
+    {
+      resolve: "@chakra-ui/gatsby-plugin",
+      options: {
+        resetCSS: true,
+        isUsingColorMode: true,
+      },
     },
-    __key: "images"
-  }]
+    "gatsby-plugin-image",
+    "gatsby-plugin-react-helmet",
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        icon: "src/images/icon.png",
+      },
+    },
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+  ],
 };
 
 export default config;
---
+++ src/@chakra-ui/gatsby-plugin/theme.js
@@ -0,0 +1,19 @@
+import { extendTheme } from "@chakra-ui/react";
+const theme = {
+  initialColorMode: "dark",
+  useSystemColorMode: true,
+  colors: {
+    primary: {
+      light: "#52c7b8",
+      main: "#009688",
+      dark: "#00675b",
+    },
+    secondary: {
+      light: "#67daff",
+      main: "#03A9F4",
+      dark: "#007ac1",
+    },
+  },
+};
+
+export default extendTheme(theme);
