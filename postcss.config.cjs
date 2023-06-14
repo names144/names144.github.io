498afeef40a73146ad0c0532fad6aa241e77a6c1
Nick Ames <names144@gmail.com>
2023-06-14 08:50:25 -0700

    ⬆️ Update deps, switch to pnpm

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -2,7 +2,7 @@ module.exports = {
   plugins: [
     require("autoprefixer"),
     require("postcss-nesting"),
-    require("@tailwindcss/nesting"),
+    require("tailwindcss/nesting"),
     require("cssnano"),
   ],
 };
