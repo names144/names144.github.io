d45cd148478eb982b5cae92346383946b61c109c
Nick Ames <names144@gmail.com>
2023-10-18 08:53:59 -0700

    ⬆️ Upgrade to Astro v3

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,10 @@
 import { defineConfig } from "astro/config";
-import image from "@astrojs/image";
-
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://names144.github.io",
   integrations: [
-    image({
-      serviceEntryPoint: "@astrojs/image/sharp",
-    }),
     tailwind(),
   ],
 });
